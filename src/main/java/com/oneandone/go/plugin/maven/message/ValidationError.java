4399a275078875f03a9ffdc1a0a063798c033e9f
Johann Böhler <johann.boehler@googlemail.com>
2015-04-16 01:25:17 +0200

    javadoc

--- src/main/java/com/oneandone/go/plugin/maven/message/ValidationError.java
+++ src/main/java/com/oneandone/go/plugin/maven/message/ValidationError.java
@@ -4,15 +4,28 @@ import com.google.gson.annotations.Expose;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+/** Representation of a validation error. */
 @EqualsAndHashCode
 public class ValidationError {
 
+    /** The error key. */
     @Expose
     private String key;
 
+    /**
+     * The error message.
+     *
+     * @return the error message
+     */
     @Expose
     @Getter private String message;
 
+    /**
+     * Constructs a validation error.
+     *
+     * @param key the error key
+     * @param message the error message
+     */
     public ValidationError(final String key, final String message) {
         this.key = key;
         this.message = message;
--- src/main/java/com/oneandone/go/plugin/maven/message/ValidationResultMessage.java
+++ src/main/java/com/oneandone/go/plugin/maven/message/ValidationResultMessage.java
@@ -5,22 +5,48 @@ import lombok.Getter;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Representation of a validation result. */
 public class ValidationResultMessage {
 
-    @Getter private List<ValidationError> validationErrors = new ArrayList<ValidationError>();
+    /**
+     * The list of validation errors.
+     *
+     * @return the list of validation errors
+     */
+    @Getter private List<ValidationError> validationErrors = new ArrayList<>();
 
+    /**
+     * Adds the specified error to the list of errors.
+     *
+     * @param validationError the validation error to add
+     */
     public void addError(final ValidationError validationError) {
         validationErrors.add(validationError);
     }
 
+    /**
+     * Adds the list of errors to the list of errors.
+     *
+     * @param validationErrors the validations errors to add
+     */
     public void addErrors(final List<ValidationError> validationErrors) {
         this.validationErrors.addAll(validationErrors);
     }
 
+    /**
+     * Returns {@code true} if any errors exist, otherwise {@code false}.
+     *
+     * @return {@code true} if any errors exist, otherwise {@code false}
+     */
     public boolean failure() {
         return !validationErrors.isEmpty();
     }
 
+    /**
+     * Returns {@code true} if no errors exists, otherwise {@code false}.
+     *
+     * @return {@code true} if no errors exists, otherwise {@code false}
+     */
     public Boolean success() {
         return !failure();
     }
