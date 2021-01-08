90f76172ad34d1abdf61e5ea3c87b285faf859d7
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2021-01-08 16:17:05 +0100

    Version description

--- .circleci/bintray-upload-version.sh
+++ .circleci/bintray-upload-version.sh
@@ -13,3 +13,15 @@ for FILE in go-maven-poller-plugin-${VERSION}.jar go-maven-poller-plugin-${VERSI
     curl -T ${FILE} -u${JFROG_USER}:${JFROG_APIKEY} https://api.bintray.com/content/sfuhrm/go-maven-poller/plugin/${VERSION}/${FILE}
 done    
 curl -X POST -u${JFROG_USER}:${JFROG_APIKEY} https://api.bintray.com/content/sfuhrm/go-maven-poller/plugin/${VERSION}/publish
+
+
+curl -X PATCH -u${JFROG_USER}:${JFROG_APIKEY} \
+https://api.bintray.com/packages/sfuhrm/go-maven-poller/plugin/versions/${VERSION} \
+-H 'Content-Type: application/json; charset=utf-8' \
+--data-binary @- << EOF
+{
+  "desc": "GoCD Maven Poller Plugin Version ${VERSION}",
+  "vcs_tag": "${VERSION}",
+  "released": "$(date --iso-8601=seconds)"
+}
+EOF
